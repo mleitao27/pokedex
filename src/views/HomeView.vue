a501ee728889f77bf27abbfb19c3aa824f5a11aa
mleitao27 <mleitao27@gmail.com>
2022-10-14 22:02:55 +0100

    tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="text-red-300">Hello</div>
+  <div class="text-2xl">Hello</div>
 </template>
